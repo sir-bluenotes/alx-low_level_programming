890f7b8d38f2b8462cb399fad8ce08daf5ddaa13
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-09-11 13:43:49 -0700

    fourth

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - main function
+*return: always 0
+*/
+int main(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
