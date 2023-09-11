d562dc438fb41743b3fb0c34cf7818b25b163e96
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-09-11 14:36:52 -0700

    many

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - main function
+*return: always 0
+*/
+int main(void)
+{
+	int i;
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar (i + '0');
+		else
+		{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	return (0);
+}
