81b57711589a613eae84e9cf61570bfa33794c58
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-09-11 13:27:30 -0700

    thrid

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main- main function
+*return: always 0
+*/
+int main(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	return (0);
+}
