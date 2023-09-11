4821e77bdd53de97b9c2d0ba34f8b16bdfd406aa
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-09-11 13:02:14 -0700

    vary2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,6 +19,6 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, m);
 	if (m < 6 && m != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
-	printf("\n")
+	printf("\n");
 	return (0);
 }
