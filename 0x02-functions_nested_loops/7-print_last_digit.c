c2d3b90f9081f6b0a745019018ccc464f4bc3efc
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-09-13 05:04:10 -0700

    jack

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_last_digit- prints last digit of a number
+*@i: function parameter
+*Return: j
+*/
+int print_last_digit(int i)
+{
+	int j;
+
+	j = i % 10;
+	if (i < 0)
+		j = -j;
+	_putchar('j' + 0);
+	return (j);
+}
