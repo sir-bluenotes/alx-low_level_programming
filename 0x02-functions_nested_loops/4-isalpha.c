c06d8b088c2e45413100234b8e28b00fd3fa89c5
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-09-13 02:54:09 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_isalpha - checks for alpha characters
+*@c: character being checked
+*Return: 0 or 1
+*/
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
