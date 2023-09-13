726e448171b70a1e54c07e8d8bf16d9bddb50d60
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-09-13 13:33:58 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,6 +13,14 @@ void times_table(void)
 		for (j = 0 ; j < 10 ; j++)
 		{
 			result = i * j;
+			if (j == 0)
+				printf("%d, ", result);
+			else
+			{
+				printf("%2d, ", result);
+				if (j != 9)
+					printf(", ");
+			}
 			printf("%2d, ", result);
 		}
 		printf("\n");
