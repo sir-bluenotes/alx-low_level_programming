8c61e2337ebc08c2984e5531df6d71518ccb58e1
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-09-14 14:17:52 -0700

    comment

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,9 +11,9 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;
+	i = 0; /* i is not increasing value */
 
-	while (i < 10)/* there is no increasing value of i */
+	while (i < 10)/* i is lower than 10 and  there is no increasing value of i */
 	{
 		putchar(i);
 	}
