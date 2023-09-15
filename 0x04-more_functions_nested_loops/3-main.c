e34a222a740d7db21cc246e7b7fab8d57d82f25a
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-09-15 15:17:33 -0700

    3

--- 0x04-more_functions_nested_loops/3-main.c
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * main - check the code
  *
@@ -5,6 +6,6 @@
  */
 int main(void)
 {
-    print_numbers();
-    return (0);
+	print_numbers();
+	return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
+int _putchar(char c);
 void print_triangle(int size);
 void print_square(int size);
 void print_diagonal(int n);
