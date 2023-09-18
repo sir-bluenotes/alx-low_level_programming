babe2a3795f27c223c9ed86f6ad4501087387733
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-09-18 11:48:07 -0700

    puts

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,8 +3,11 @@
 #include <unistd.h>
 #include <stdio.h>
 
+void _puts(char *str);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 int _strlen(char *s);
+int _putchar(char c);
+
 #endif
 
