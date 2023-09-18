682bdff152617c9b8d2f78d24ed348311221d67f
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-09-18 11:03:18 -0700

    point

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*reset_to_98 - resets the function to 98
+*@n: function parameter
+*Return: void
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdio.h>
+
+void reset_to_98(int *n);
+
+#endif
+
