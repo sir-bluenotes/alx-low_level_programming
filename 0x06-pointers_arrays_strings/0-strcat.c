038eb24f1cd13cf97f949285cc609becb259a8f4
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-09-22 09:23:00 -0700

    conc

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+**_strcat - function joins two strings
+*@dest: input parameter
+*@src: function parameter
+*Return: character
+*/
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
