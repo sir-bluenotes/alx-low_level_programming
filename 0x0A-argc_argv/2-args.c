911eb6fab7e2ee408ea932fbe9ed8d76dfac22c7
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-10-27 16:54:53 -0700

    anotha arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - prints all arguments
+*@argc:counts arguments
+*@argv: argument pointe
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
