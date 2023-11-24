5b694f5c2bf7abe1eaf61421df29af25eb68e966
sir-bluenotes <bznengomasha@usiu.ac.ke>
2023-11-24 14:26:57 -0800

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,36 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
-*binary_to_uint - function to convert binary number
-*@b: pointer to a string with binary num
+* binary_to_uint - a program that converts binary numbers
+*@b: a character pointer to a string
 *Return: 0
 */
 
 unsigned int binary_to_uint(const char *b)
 {
-	int a;
-	unsigned int num;
+	unsigned int len, i, j, sum, pow;
+	int base;
 
-	num = 0;
-	if (!b)
+	base = 2;
+	sum = 0;
+	pow = 1;
+
+	if (b == NULL)
 		return (0);
-	for (a = 0; b[a] != '\0'; a++)
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	if (len == 1 && (b[0] == '0' || b[0] == '1'))
+		return (b[0] - 48);
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[a] != '\0' && b[a] != '1')
-			return (0);
+		if (b[i] != '0' && b[i] != '1')
+		return (0);
+		for (j = len - 1; j > 0; j--)
+		pow = pow * base;
+		sum += (pow * (b[i] - 48));
+		len--;
+		pow = 1;
 	}
-	for (a = 0; b[a] != '\0'; a++)
-	{
-		num <<= 1;
-		if (b[a] == '1')
-			num += 1;
-	}
-	return (num);
+	return (sum);
 }
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+*_pow - calculates num to power
+*@base: base num
+*@power: power num
+*Return: 0
+*/
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
+}
+
+/**
+*print_binary - prints a binary num
+*@n: number printed
+*Return: void
+*/
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar(0);
+		}
+		divisor >>= 1;
+	}
+}
